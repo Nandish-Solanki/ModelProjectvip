6e66b0c5d790d2fbc9b134f4ca9a52f51219c280
nandish-solanki <nandishsolanki13102004@gmail.com>
2025-07-22 11:14:41 +0530

    database working

--- backend/database/connect.js
+++ backend/database/connect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dburl = process.env.DATABASE;
+const dburl = process.env.MONGO_URI;
 
 const mydatabase = mongoose.connect(dburl).then(()=>{
     console.log("db connected successfully");
